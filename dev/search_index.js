d6321e1b5fca3ed6c1af0d2035cad26dc5f0c77d
Documenter.jl <documenter@juliadocs.github.io>
2022-01-19 21:39:17 +0000

    build based on f3dcec9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ParadigMagic","category":"page"},{"location":"#ParadigMagic","page":"Home","title":"ParadigMagic","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ParadigMagic.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ParadigMagic]","category":"page"}]
+}
